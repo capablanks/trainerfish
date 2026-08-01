5d4c12fbaea19b5a8460a97d51e3907e672a7768
capablanks <anthony.orbe@gmail.com>
2026-08-01 23:00:48 +0800

    Fix Lichess TV Kotlin imports

--- app/src/main/java/com/tonorbe/trainerfish/LichessTvScreen.kt
+++ app/src/main/java/com/tonorbe/trainerfish/LichessTvScreen.kt
@@ -17,7 +17,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.rememberScrollState
@@ -49,7 +48,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.github.bhlangonijr.chesslib.Board
-import com.github.bhlangonijr.chesslib.MoveGenerator
+import com.github.bhlangonijr.chesslib.move.MoveGenerator
 import com.github.bhlangonijr.chesslib.PieceType as LibPieceType
 import com.github.bhlangonijr.chesslib.Side
 import com.github.bhlangonijr.chesslib.move.Move as LibMove
